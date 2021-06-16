d65b8152624768feb736b9ec4dfd9e60716a041a
ETRI Open Source Center <48229542+osc-etri@users.noreply.github.com>
2021-06-16 14:45:56 +0900

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,11 +2,13 @@ import pytest
 
 from myapp.app import multiply_by_two, divide_by_two
 
+
 @pytest.fixture
 def numbers():
     a = 10
     b = 20
-    return [a,b]
+    return [a, b]
+
 
 class TestApp:
     def test_multiplication(self, numbers):
