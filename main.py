e629c5f2bd205ec73e924790bdbb50862bc84d6b
ETRI Open Source Center <48229542+osc-etri@users.noreply.github.com>
2021-06-16 14:44:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import sys
 
 from myapp.app import multiply_by_two, divide_by_two
 
+
 def main():
     num = 3
     print("The double of %d is %d" % (num, multiply_by_two(num)))
     print("The half of %d is %d" % (num, divide_by_two(num)))
     sys.exit(0)
 
+    
 if __name__ == "__main__":
     main()
