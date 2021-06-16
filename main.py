b32033063b22f7b8c71d495a92b7c54bfe70a4f5
ETRI Open Source Center <48229542+osc-etri@users.noreply.github.com>
2021-06-16 14:27:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from myapp.app import multiply_by_two, divide_by_two
 
 def main():
-    num = int(input("Insert a number: "))
+    num = 3
     print("The double of %d is %d" % (num, multiply_by_two(num)))
     print("The half of %d is %d" % (num, divide_by_two(num)))
     sys.exit(0)
