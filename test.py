0e321004d97fd83df5b247c970c81a5250b759f9
ETRI Open Source Center <48229542+osc-etri@users.noreply.github.com>
2021-06-16 13:22:21 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,28 @@
-def func(x):
-    return x + 1
+  
+import pytest
 
-def test_func():
-    assert func(3) == 4
+
+def test_parameterization(letter):
+    print("\n   Running test_parameterization with {}".format(letter))
+
+
+def test_modes(mode):
+    print("\n   Running test_modes with {}".format(mode))
+
+
+@pytest.fixture(params=["a", "b", "c", "d", "e"])
+def letter(request):
+    """
+    Fixtures with parameters will run once per param
+    (You can access the current param via the request fixture)
+    """
+    yield request.param
+
+
+@pytest.fixture(params=[1, 2, 3], ids=['foo', 'bar', 'baz'])
+def mode(request):
+    """
+    Fixtures with parameters will run once per param
+    (You can access the current param via the request fixture)
+    """
+    yield request.param
